72a340b37eb75b0a5e55840acea5b19256f93137
Manish Ranjan <manishranjandbg@gmail.com>
2021-01-03 11:10:56 +0530

    Updates

---
+++ precache-manifest.525eca6d534e2fe4712f82aebaaffe3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be3963d077d67722fa4397c7f9d5e0d",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "2b35497123fbab80b931",
+    "url": "/COVID-19-Tracker/static/css/main.55338505.chunk.css"
+  },
+  {
+    "revision": "9b3ebe13b2bbc0ac5183",
+    "url": "/COVID-19-Tracker/static/js/2.26eb6c22.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/COVID-19-Tracker/static/js/2.26eb6c22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b35497123fbab80b931",
+    "url": "/COVID-19-Tracker/static/js/main.43ffad15.chunk.js"
+  },
+  {
+    "revision": "69bcf36b4ce849b7d724",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.bd6efa81.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
